2ac886b238e44249469030723b589cb79945811e
André Rodrigues <andrerodrigues@catenda.no>
2022-09-10 17:59:42 +0100

    add more information

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,6 +9,9 @@
                     &
                 </div>
                 <div>André Filipe</div>
+                <h2 class="date">
+                    <img src="lavender.png"><span>19 de agosto de 2023</span><img src="lavender.png" class="right-lavander">
+                </h2>
             </a>
         </h1>
     </section>
@@ -28,22 +31,38 @@
         background-image: linear-gradient(180deg, #a5b4fc, transparent 23%), linear-gradient(180deg, transparent 47%, #a5b4fc), url('static/hero-pic.jpeg');
         background-size: cover;
         background-position: top;
-        box-shadow: -1px -20px 21px 0px rgba(165,180,252,0.75) inset;
+        box-shadow: -1px -10px 21px 0px rgba(165, 180, 252, 0.75) inset;
     }
 }
+
 .names {
     font-family: Lucida Caligraphy;
+    @apply w-full;
     @apply font-bold;
     @apply text-gray-100;
     @apply text-center;
-    @apply absolute top-1/2 left-1/2;
-    transform: translate(-50%, calc(150px - 60px));
+    @apply absolute left-1/2;
+    transform: translate(-50%);
     font-size: 28px;
+    bottom: 70px;
 
     @media screen(md) {
+        bottom: 50px;
         @apply text-5xl;
-        transform: translate(-50%, 20px);
     }
+}
 
+.date {
+    @apply mt-6;
+    @apply flex items-center justify-center;
+}
+
+img {
+    width: 64px;
+    height: 64px;
+}
+
+.right-lavander {
+    transform: rotateY(180deg);
 }
 </style>
